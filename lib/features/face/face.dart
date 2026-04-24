4ca0cdcbe48d0bde5de08f82532adbe426e75148
Ezzeldeen Mohamed Abdelwahab <ezzeldeen20052018@gmail.com>
2026-04-24 18:14:24 +0300

    connect screens

---
+++ lib/features/face/face.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FaceScreen extends StatefulWidget {
+  const FaceScreen({super.key});
+
+  @override
+  State<FaceScreen> createState() => _FaceScreenState();
+}
+
+class _FaceScreenState extends State<FaceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
